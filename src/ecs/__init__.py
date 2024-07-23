02e8fc6ad73fed898e65df8d7b09f5d66a2b1850
Alexis LUCIEN <alexis.lucien@outlook.fr>
2024-07-23 20:54:57 +0200

    first commit

---
+++ src/ecs/__init__.py
@@ -0,0 +1,25 @@
+from .core import (
+    Entity,
+    Component,
+    Query,
+    Criteria,
+    HasId,
+    HasComponent,
+    HasNotComponent,
+    HasValue,
+    HasValues,
+    Has,
+)
+
+__all__ = [
+    "Entity",
+    "Component",
+    "Query",
+    "Criteria",
+    "HasId",
+    "HasComponent",
+    "HasNotComponent",
+    "HasValue",
+    "HasValues",
+    "Has",
+]
